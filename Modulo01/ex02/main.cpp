e73b7fc7d327b1f324337e63b371d57b0c51fbab
Luzizila Helena Nzila <lnzila@c1r1s8.42luanda.com>
2025-04-29 22:44:50 +0100

    done

---
+++ Modulo01/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnzila <lnzila@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 08:49:33 by lnzila            #+#    #+#             */
+/*   Updated: 2025/04/21 08:22:35 by lnzila           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	if (ac != 1)
+	{
+		std::cout << "Invalid! Run using only ./brain" << std::endl;
+		return (1);
+	}
+	
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << std::setw(40) << "The memory address of the string is " << &string << std::endl;
+	std::cout << std::setw(40) << "The memory address held by stringPTR is " << stringPTR << std::endl;
+	std::cout << std::setw(40) << "The memory address held by stringREF is " << &stringREF << std::endl;
+	std::cout << std::setw(40) << "The value of the string is " << string << std::endl;
+	std::cout << std::setw(40) << "The value pointed to by stringPTR is " << *stringPTR << std::endl;
+	std::cout << std::setw(40) << "The value pointed to by stringREF is " << stringREF << std::endl;
+	
+	return (0);
+}
